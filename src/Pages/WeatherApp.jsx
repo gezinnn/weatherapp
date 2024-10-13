ca228f606c0c2bc39936a4e70cde2ac004126e71
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2024-10-12 21:34:41 -0300

    fixed weather undefined error

--- src/Pages/WeatherApp.jsx
+++ src/Pages/WeatherApp.jsx
@@ -2,18 +2,20 @@ import React, { useState } from "react";
 import styles from "../Pages/WeatherApp.module.css";
 
 const api = {
-  key: "",
+  key: "aa55592c2af69d3005493afabc90cb08",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
 export default function WeatherApp() {
   const [ search, setSearch ] = useState("");
+  const [ weather, setWeather ] = useState({})
 
   const searchPressed = () => {
     fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
-        console.log(result);
+        console.log(result)
+        setWeather(result);
       });
   };
 
@@ -23,12 +25,30 @@ export default function WeatherApp() {
         <h1>Weather App</h1>
       </div>
       <div className={styles.appcontent}>
+        <div className={styles.inputSearch}>
         <input
           type="text"
           onChange={(e) => setSearch(e.target.value)}
           placeholder="Procure uma Cidade"
         />
+      
         <button onClick={searchPressed}>Pesquisar</button>
+
+        </div>
+
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <p>{weather.name}</p>
+            <p>{weather.main.temp}°C</p>
+            <p>{weather.weather[0].main}</p>
+            <p>{weather.weather[0].description}</p>
+
+
+          </div>
+        ) : (
+          ""
+        )}
+
 
 
       </div>

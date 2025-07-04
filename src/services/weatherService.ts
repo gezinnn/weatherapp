840859d7dae422bea0b1efd8b8b8fe573844e15b
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-03 23:00:38 -0300

    feat: adiciona camada de serviço para buscar dados do clima via OpenWeather

---
+++ src/services/weatherService.ts
@@ -0,0 +1,13 @@
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+
+export async function getCurrentWeather(city: string) {
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=pt_br`
+  );
+
+  if (!res.ok) {
+    throw new Error("Cidade Não Encontrado");
+  }
+
+  return res.json();
+}

0c30a7a2be87964dbb024580d7a82eb70ef942c2
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-14 16:55:07 -0300

    feat: implementar busca e exibição de clima atual e previsão para 5 dias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,113 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
+import WeatherCard from "../components/WeatherCard";
+import ForecastCard from "../components/ForecastCard";
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+import {
+  getCurrentWeather,
+  getForecastByCity,
+} from "../services/weatherService";
+import type { ForecastDay } from "../components/ForecastCard";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [forecast, setForecast] = useState<ForecastDay[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  type ForecastAPIItem = {
+    dt_txt: string;
+    main: {
+      temp: number;
+    };
+    weather: {
+      icon: string;
+      description: string;
+    }[];
+  };
+
+  const fetchWeather = async () => {
+    setLoading(true);
+    setError("");
+
+    try {
+      const [weatherData, forecastData] = await Promise.all([
+        getCurrentWeather(city),
+        getForecastByCity(city),
+      ]);
+
+      setWeather(weatherData);
+
+      const dailyMap: Record<string, ForecastAPIItem[]> = {};
+      forecastData.list.forEach((item: ForecastAPIItem) => {
+        const date = item.dt_txt.split(" ")[0];
+        if (!dailyMap[date]) dailyMap[date] = [];
+        dailyMap[date].push(item);
+      });
+
+      const dailyForecast = Object.entries(dailyMap)
+        .slice(0, 5)
+        .map(([date, entries]) => {
+          const temps = entries.map((e) => e.main.temp);
+          const max = Math.max(...temps);
+          const min = Math.min(...temps);
+          const midday =
+            entries.find((e) => e.dt_txt.includes("12:00:00")) || entries[0];
+
+          return {
+            date,
+            max,
+            min,
+            icon: midday.weather[0].icon,
+            description: midday.weather[0].description,
+          };
+        });
+
+      setForecast(dailyForecast);
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Erro desconhecido");
+      }
+      setWeather(null);
+      setForecast([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <main className="min-h-screen flex justify-center px-4 py-8">
+      <div className="w-full max-w-3xl">
+        <h1 className="text-4xl font-bold text-center mb-4">WeatherWise 🌤️</h1>
+
+        <SearchBar city={city} setCity={setCity} onSearch={fetchWeather} />
+
+        <div className="flex flex-col mt-6 items-center">
+          {loading && <Loader />}
+          {error && <Error message={error} />}
+          {weather && <WeatherCard data={weather} />}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        {forecast.length > 0 && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-6">
+            {forecast.map((day, index) => (
+              <ForecastCard
+                key={index}
+                date={day.date}
+                icon={day.icon}
+                description={day.description}
+                max={day.max}
+                min={day.min}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </main>
   );
 }

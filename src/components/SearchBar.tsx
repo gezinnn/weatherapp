ee31488578d0aa1bcb6635128804587c936feb2e
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-14 15:41:47 -0300

    feat: instala o shadcn e adiciona e button e input e atualiza a SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,3 +1,6 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 type Props = {
   city: string;
   setCity: (value: string) => void;
@@ -6,20 +9,19 @@ type Props = {
 
 export default function SearchBar({ city, setCity, onSearch }: Props) {
   return (
-    <div className="flex flex-col sm:flex-rom gap-2 mb-5">
-      <input
+    <div className="flex sm:flex-rom gap-2 mb-3">
+      <Input
         type="text"
         value={city}
         onChange={(e) => setCity(e.target.value)}
         placeholder="Digite o nome da cidade"
         className="px-4 py-2 rounded text-black"
       />
-      <button
+      <Button
         onClick={onSearch}
-        className="bg-white text-black px-4 py-2 rounded hover:bg-gray-200"
       >
         Buscar
-      </button>
+      </Button>
     </div>
   );
 }

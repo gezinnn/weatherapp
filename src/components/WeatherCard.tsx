c446ce52c5c6f01939fcb39cd28e16454cfe1eac
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-14 16:53:23 -0300

    refactor: tipar componente WeatherCard com WeatherData e remover uso de any

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,18 +1,22 @@
+import { WeatherData } from "@/types/weather";
+
 type Props = {
-  data: any;
+  data: WeatherData;
 };
 
 export default function WeatherCard({ data }: Props) {
   return (
-    <div className="bg-white bg-opacity-10 p-6 rounded-xl text-center max-w-sm">
+    <div className=" w-lg bg-white text-black p-6 rounded-xl text-center border shadow-md hover:scale-103 transition-transform duration-300">
       <h2 className="text-2xl font-semibold mb-2">
         {data.name}, {data.sys.country}
       </h2>
-      <p className="text-6xl font-bold">{Math.round(data.main.temp)}°C</p>
-      <p className="capitalize">{data.weather[0].description}</p>
-      <p>Sensação térmica: {Math.round(data.main.feels_like)}°C</p>
-      <p>Umidade: {data.main.humidity}%</p>
-      <p>Vento: {data.wind.speed} km/h</p>
+      <div className="flex">
+        <p className="text-6xl font-bold">{Math.round(data.main.temp)}°C</p>
+        <p className="capitalize">{data.weather[0].description}</p>
+        <p>Sensação térmica: {Math.round(data.main.feels_like)}°C</p>
+        <p>Umidade: {data.main.humidity}%</p>
+        <p>Vento: {data.wind.speed} km/h</p>
+      </div>
     </div>
   );
 }
